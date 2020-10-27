190d347c231a7dcab9386b325eba0418d3e37ff8
Ivan Chinea <chitenavi@gmail.com>
2020-10-27 16:56:44 +0000

    add: init users to DB

--- server/dev-data/init-db.js
+++ server/dev-data/init-db.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const readline = require('readline');
 const fs = require('fs');
 const conn = require('../utils/connectMoonDB');
 const Advert = require('../models/advertModel');
+const User = require('../models/userModel');
 
 // Read JSON file with init adverts
 const adverts = JSON.parse(fs.readFileSync('./data/adverts.json', 'utf-8'));
+// Read JSON file with one user for development
+// const users = JSON.parse(fs.readFileSync('./data/users.json', 'utf-8'));
 
 // Delete all data and reload the initial adverts
 const initAdvertsDB = async () => {
@@ -22,6 +26,39 @@ const initAdvertsDB = async () => {
     console.log(`There was an error!: ${err}`);
     process.exit(1);
   }
+};
+
+// Delete all users and reload the initial users, one user for
+// development is mandatory
+const initUsersDB = async () => {
+  console.log('Emptying users collection...');
+  await User.deleteMany();
+
+  /*
+  let arrHashPass = [];
+  users.forEach(user => {
+    arrHashPass.push(User.hashPassword(user.password));
+  });
+  arrHashPass = await Promise.all(arrHashPass);
+  for (let i = 0; i < users.length; i += 1) {
+    users[i].password = arrHashPass[i];
+  }
+  console.log(users);
+  */
+
+  console.log('Loading users...');
+  const users = await User.insertMany([
+    {
+      username: process.env.DEV_USERNAME,
+      email: process.env.DEV_USER_EMAIL,
+      avatar: process.env.DEV_USER_AVATAR,
+      password: await User.hashPassword(process.env.DEV_USER_PASS),
+    },
+  ]);
+  console.log(users);
+  console.log(
+    `Users successfully loaded!. ${users.length} users have been created.`
+  );
 };
 
 function askUser(askText) {
@@ -48,9 +85,11 @@ conn.once('open', async () => {
     }
 
     await initAdvertsDB();
+    await initUsersDB();
 
     // close connection
     conn.close();
+    process.exit();
   } catch (err) {
     console.log(`There was an error!: ${err}`);
     process.exit(1);
