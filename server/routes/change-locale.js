cf35bb9691cb0a8ee57aa9e041b82d7293ac61b1
Ivan Chinea <chitenavi@gmail.com>
2020-11-04 11:50:05 +0000

    fix: lang pages titles

--- server/routes/change-locale.js
+++ server/routes/change-locale.js
@@ -4,17 +4,17 @@ const router = express.Router();
 
 /* GET /change-locale/:locale */
 router.get('/:locale', (req, res, next) => {
-  // Recuperar el locale que nos pasan
+  // Get locale param
   const { locale } = req.params;
 
-  // guardar la página de donde precede el usuario
-  const volverA = req.get('referer');
+  // save page from user comming
+  const returnTo = req.get('referer');
 
-  // establecer la cookie en la respuesta con el nuevo locale
+  // new response cookie with locale
   res.cookie('nodepop-locale', locale, { maxAge: 1000 * 60 * 60 * 24 * 20 });
 
-  // redirigir al usuario a donde venía
-  res.redirect(volverA);
+  // redirect to same page
+  res.redirect(returnTo);
 });
 
 module.exports = router;
--- server/utils/jwtAuth.js
+++ server/utils/jwtAuth.js
@@ -10,14 +10,15 @@ module.exports = function () {
     const tokenJWT =
       req.get('Authorization') || req.query.token || req.body.token;
 
-    // if no token then return
+    // if no token then return error 401
     if (!tokenJWT) {
       return next(createError(401, 'No token provided'));
     }
 
     // verify the received token
     jwt.verify(tokenJWT, process.env.JWT_SECRET, (err, payload) => {
-      if (err) return next(err);
+      // if TokenExpiredError or JsonWebTokenError or NotBeforeError
+      if (err) return next(createError(401, err.message));
       req.apiAuthUserId = payload._id;
       next();
     });
